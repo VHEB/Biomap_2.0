cd19a85aa7fd4c24de38ccde090d111f9f3a256f
VHEB <vheb0316@gmail.com>
2026-05-31 20:51:24 -0300

    fix: login sendo realizado atráves do google

---
+++ services/auth.js
@@ -0,0 +1,51 @@
+import { auth, db } from "../firebase/firebase-config.js";
+
+import {
+    GoogleAuthProvider,
+    signInWithPopup,
+    signOut,
+    onAuthStateChanged
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+import {
+    doc,
+    setDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const provider = new GoogleAuthProvider();
+
+
+export async function loginWithGoogle() {
+
+    const result =
+        await signInWithPopup(auth, provider);
+
+    const user = result.user;
+
+    await setDoc(
+        doc(db, "users", user.uid),
+        {
+            nome: user.displayName,
+            email: user.email,
+            photoURL: user.photoURL,
+            provider: "google"
+        },
+        { merge: true }
+    );
+
+    return user;
+}
+
+
+export async function logoutUser() {
+
+    await signOut(auth);
+
+}
+
+
+export function observeAuth(callback) {
+
+    onAuthStateChanged(auth, callback);
+
+}
